ecac18ff1a0ceaba6fbe279eecf48f91314815bf
petauday123 <96293898+petauday123@users.noreply.github.com>
2024-07-26 21:40:47 +0530

    first commit

---
+++ src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent {
+  isNavOpen = false;
+
+  openNav() {
+    this.isNavOpen = true;
+  }
+
+  closeNav() {
+    this.isNavOpen = false;
+  }
+}
